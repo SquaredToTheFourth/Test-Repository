7ee865d2192215f56c02e3a125d9a6d34c7f136a
SquaredToTheFourth <mason.connor06@gmail.com>
2022-11-13 01:51:59 -0600

    Second Ever Commit!

---
+++ Basics/SecondCommit.java
@@ -0,0 +1,6 @@
+public class SecondCommit {
+    public static void main(String[] args){
+        boolean commitingIsFun = !false ? true : false;
+        System.out.println("Is Git Fun? " + commitingIsFun);
+    }
+}
